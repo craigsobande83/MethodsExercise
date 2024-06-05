e06718f678e02dc2c963274bd4dff7bcbac873f8
Craig Sobande <craigsobande83@gmail.com>
2024-06-05 16:21:54 -0400

    Finished Profile

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello what is your name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi,{userName}, what is your favorite color ? ");
+              var color = Console.ReadLine();
+
+            Console.WriteLine($" Nice, {color} is an awesome color! what is your favorite animal ?") ;
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Great, Now what is your favorite band ");
+            var band = Console.ReadLine();
+
+            Console.WriteLine($" Thanks, {userName}! Here is your Login profile") ;
+
+            Console.WriteLine("-----------------------------");
+
+            Console.WriteLine($"Name: {userName}");
+            Console.WriteLine($"Favorite Color :{color} ");
+            Console.WriteLine($" Favorite Animal : {animal}");
+            Console.WriteLine($"Favorite Band: {band}");
+
+
         }
     }
 }
