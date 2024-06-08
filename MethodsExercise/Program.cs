843583ce6a8fdcbffcd30ace9a7153a15bfc57c7
Craig Sobande <craigsobande83@gmail.com>
2024-06-08 18:25:15 -0400

    Completed Exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,21 +2,41 @@
 {
     public class Program
     {
+
+        public static int sum(int num1, int num2)
+
+        {
+            return num1 + num2;
+        }
+
+        public static int sum(int num1, int num2, int num3)
+        {
+            return num1+ num2 + num3;
+        }
+
+        public static int sum(int num1, int num2 , int num3 , int num4, int num5)
+        {
+            return num1 + num2 + num3 + num4 + num5;
+
+         }
+
+
         static void Main(string[] args)
+
         {
             Console.WriteLine("Hello what is your name?");
             var userName = Console.ReadLine();
 
             Console.WriteLine($"Hi,{userName}, what is your favorite color ? ");
-              var color = Console.ReadLine();
+            var color = Console.ReadLine();
 
-            Console.WriteLine($" Nice, {color} is an awesome color! what is your favorite animal ?") ;
+            Console.WriteLine($" Nice, {color} is an awesome color! what is your favorite animal ?");
             var animal = Console.ReadLine();
 
             Console.WriteLine("Great, Now what is your favorite band ");
             var band = Console.ReadLine();
 
-            Console.WriteLine($" Thanks, {userName}! Here is your Login profile") ;
+            Console.WriteLine($" Thanks, {userName}! Here is your Login profile");
 
             Console.WriteLine("-----------------------------");
 
@@ -25,7 +45,23 @@
             Console.WriteLine($" Favorite Animal : {animal}");
             Console.WriteLine($"Favorite Band: {band}");
 
+            int answer1 = sum(2,4 );
+            int answer2 = sum(2, 4,6);
+            int answer3 = sum(1,1,1,1,1);
+            Console.WriteLine(answer1);
+            Console.WriteLine(answer2);
+            Console.WriteLine(answer3);
 
         }
+
+
+
+
     }
+
+
+
+
+
+
 }
